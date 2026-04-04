058b11a13a2b51b4ad5e4540ab227e4853c3a6c1
rakhzar <mnv36ru@gmail.com>
2026-04-04 15:00:11 +0300

    fix(HeaderMenu): add stable border to prevent link shift on navigation

--- components/header/HeaderMenu.vue
+++ components/header/HeaderMenu.vue
@@ -13,8 +13,19 @@
 }
 
 .header__menu a {
+  font-size: 16px;
   line-height: 27px;
   color: var(--color-black);
   text-decoration: none;
+  padding-bottom: 24px;
+  border-bottom: 2px solid transparent;
+}
+
+.header__menu a:hover {
+  color: var(--color-dark-gray);
+}
+
+.header__menu a.router-link-active {
+  border-bottom: 2px solid var(--color-black);
 }
 </style>
