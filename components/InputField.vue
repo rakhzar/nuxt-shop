52101349c9c49418a3ae1f770734a10be9111223
rakhzar <mnv36ru@gmail.com>
2026-04-05 14:02:35 +0300

    refactor(InputField): use CSS Modules for dynamic variant classes

--- components/InputField.vue
+++ components/InputField.vue
@@ -7,10 +7,10 @@ const data = defineModel<string>();
 </script>
 
 <template>
-  <input v-model="data" class="input" :class="variant" />
+  <input v-model="data" :class="[$style[variant], $style.input]" />
 </template>
 
-<style scoped>
+<style module>
 .input {
   padding: 12px 0;
   background: none;
