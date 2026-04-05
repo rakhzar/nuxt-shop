0b39d7f4e19a0fe3edaec0657638a70aeeb75a72
rakhzar <mnv36ru@gmail.com>
2026-04-05 14:00:34 +0300

    feat: add page fade transitions and layout spacing

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <main>
+      <NuxtPage
+        :transition="{
+          name: 'page',
+          mode: 'out-in',
+        }"
+      />
+    </main>
   </NuxtLayout>
 </template>
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+
+main {
+  margin: 0 auto;
+  max-width: 1248px;
+  padding: 40px 0;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
