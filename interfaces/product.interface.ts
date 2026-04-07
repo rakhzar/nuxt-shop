4c1741df4311aff8b896be18929dcaf7a407fd1d
rakhzar <mnv36ru@gmail.com>
2026-04-07 14:28:45 +0300

    feat(interfaces): add Product interface with full type definitions

---
+++ interfaces/product.interface.ts
@@ -0,0 +1,16 @@
+import type { Category } from './category.interface';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  short_description: string;
+  long_description: string;
+  sku: number;
+  discount: number;
+  images: string[];
+  category_id: number;
+  category: Category;
+  created_at: Date;
+  updated_at: Date;
+}
