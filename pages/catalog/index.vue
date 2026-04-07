0be550db0e552ef500d7b5ad1219e07a4296151a
rakhzar <mnv36ru@gmail.com>
2026-04-07 10:55:30 +0300

    feat(catalog): add SelectField component with props data

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+// import type { GetCategoriesResponse } from '~/interfaces/category.interface';
+
+// const config = useRuntimeConfig();
+// const API_URL = config.public.apiurl;
+// const input = ref('');
+// const select = ref('');
+
+// await useAsyncData<GetCategoriesResponse>('categoreis', () => $fetch(API_URL + '/categories'), {
+//   watch: [input],
+// });
+</script>
+
 <template>
-  <div>Catalog</div>
+  <div>
+    Catalog
+    <SelectField
+      v-model="select"
+      :options="[
+        {
+          value: '',
+          label: 'Категория',
+        },
+        {
+          value: '1',
+          label: 'Первый',
+        },
+        {
+          value: '2',
+          label: 'Второй',
+        },
+      ]"
+    />
+  </div>
 </template>
