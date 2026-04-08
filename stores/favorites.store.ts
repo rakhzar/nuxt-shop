1c0b852af04b38b4fd8c9081d007d943d2c2e2ea
rakhzar <mnv36ru@gmail.com>
2026-04-08 18:48:04 +0300

    feat: add fetch, toggle, and isFavorite to favorites store

--- stores/favorites.store.ts
+++ stores/favorites.store.ts
@@ -1,10 +1,7 @@
 import { defineStore } from 'pinia';
-import type { GetCategoriesResponse } from '~/interfaces/category.interface';
 
 export const useFavoritesStore = defineStore('favorites', () => {
   const favoriteIds = ref<number[]>([]);
-  const config = useRuntimeConfig();
-  const API_URL = config.public.apiurl;
 
   function addFavorite(id: number) {
     if (!favoriteIds.value.includes(id)) {
@@ -16,10 +13,17 @@ export const useFavoritesStore = defineStore('favorites', () => {
     favoriteIds.value = favoriteIds.value.filter((item) => item != id);
   }
 
-  async function fetchFavorites() {
-    const data = await $fetch<GetCategoriesResponse>(API_URL + '/categories');
-    favoriteIds.value = data.categories.map((c) => c.id);
+  function isFavorite(id: number) {
+    return favoriteIds.value.find((f) => f == id);
   }
 
-  return { favoriteIds, addFavorite, removeFromFavorite, fetchFavorites };
+  function toggleFavorite(id: number) {
+    if (!favoriteIds.value.includes(id)) {
+      favoriteIds.value.push(id);
+      return;
+    }
+    favoriteIds.value = favoriteIds.value.filter((item) => item != id);
+  }
+
+  return { favoriteIds, addFavorite, removeFromFavorite, toggleFavorite, isFavorite };
 });
