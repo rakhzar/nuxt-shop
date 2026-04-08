fb55235bfbd038642f908973bbcc7dc267ba5e9e
rakhzar <mnv36ru@gmail.com>
2026-04-08 17:59:15 +0300

    feat: create AddFavorite component with id and isShow props

---
+++ components/AddFavorite.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { id, isShow } = defineProps<{
+  id: number;
+  isShow: boolean;
+}>();
+</script>
+
+<template>
+  <button v-show="isShow" class="fav-button">
+    <Icon name="icons:favorite-add" size="19px" />
+  </button>
+</template>
+
+<style scoped>
+.fav-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
