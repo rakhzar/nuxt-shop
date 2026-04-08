67e76d7a3f6ea8b026e963c8c1c7892e8f9cf3ec
rakhzar <mnv36ru@gmail.com>
2026-04-08 14:51:29 +0300

    feat: add debounce to watch for category_id and search

--- composables/useCatalogFilters.ts
+++ composables/useCatalogFilters.ts
@@ -1,3 +1,5 @@
+import { useDebounceFn } from '@vueuse/core';
+
 export function useCatalogFilters() {
   const route = useRoute();
   const router = useRouter();
@@ -5,9 +7,13 @@ export function useCatalogFilters() {
   const category_id = ref(route.query.category_id?.toString() ?? '');
   const search = ref(route.query.search?.toString() ?? '');
 
-  watchEffect(() => {
-    router.replace({ query: { category_id: category_id.value, search: search.value } });
+  watch([category_id, search], () => {
+    changeRoute(category_id, search);
   });
+
+  const changeRoute = useDebounceFn((category_id, search) => {
+    router.replace({ query: { category_id: category_id.value, search: search.value } });
+  }, 100);
 
   const query = computed(() => ({
     limit: route.query.limit ?? 20,
