d36b45e4f691d6fb0b903f590d3f483d64572d6b
rakhzar <mnv36ru@gmail.com>
2026-04-08 18:49:04 +0300

    feat: wire up toggle logic in AddFavorite component

--- components/AddFavorite.vue
+++ components/AddFavorite.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-const { id, isShow } = defineProps<{
+const { id, isShown } = defineProps<{
   id: number;
-  isShow: boolean;
+  isShown: boolean;
 }>();
+const favoriteStore = useFavoritesStore();
 </script>
 
 <template>
-  <button v-show="isShow" class="fav-button">
+  <button v-show="isShown || favoriteStore.isFavorite(id)" class="fav-button" @click.stop.prevent="() => favoriteStore.toggleFavorite(id)">
     <Icon name="icons:favorite-add" size="19px" />
   </button>
 </template>
