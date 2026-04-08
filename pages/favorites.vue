94fe64370c22b05d39739459fba1595ae1d2d515
rakhzar <mnv36ru@gmail.com>
2026-04-08 18:51:48 +0300

    refactor: clean up favorites page template

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-const favoriteStore = useFavoritesStore();
-await callOnce('fetchFavorites', () => favoriteStore.fetchFavorites(), {
-  mode: 'navigation',
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>{{ favoriteStore.favoriteIds[0] }}</div>
+  <div>Fav</div>
 </template>
