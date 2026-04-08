8240c1e0d2a00111ce61285f6617368776322a41
rakhzar <mnv36ru@gmail.com>
2026-04-08 11:41:02 +0300

    feat: add watchEffect for search query parameter sync

--- composables/useCatalogFilters.ts
+++ composables/useCatalogFilters.ts
@@ -3,16 +3,18 @@ export function useCatalogFilters() {
   const router = useRouter();
 
   const category_id = ref(route.query.category_id?.toString() ?? '');
+  const search = ref(route.query.search?.toString() ?? '');
 
-  watch(category_id, () => {
-    router.replace({ query: { category_id: category_id.value } });
+  watchEffect(() => {
+    router.replace({ query: { category_id: category_id.value, search: search.value } });
   });
 
   const query = computed(() => ({
     limit: route.query.limit ?? 20,
     offset: route.query.offset ?? 0,
     category_id: route.query.category_id || undefined,
+    search: route.query.search || undefined,
   }));
 
-  return { category_id, query };
+  return { category_id, search, query };
 }
