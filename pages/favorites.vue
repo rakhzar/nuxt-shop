f29bd540fdbf1a21225b0d610c628740f4aa1c19
rakhzar <mnv36ru@gmail.com>
2026-04-09 10:00:27 +0300

    feat: add favorites page with product grid

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Product } from '~/interfaces/product.interface';
+
+const favoriteStore = useFavoritesStore();
+const config = useRuntimeConfig();
+const API_URL = config.public.apiurl;
+const products = ref<Product[]>();
+
+watchEffect(async () => {
+  const data = await Promise.all(
+    favoriteStore.favoriteIds.map((id) => {
+      return $fetch<{ product: Product }>(API_URL + '/products/' + id);
+    })
+  );
+  products.value = data.map((el) => el.product);
+});
+</script>
 
 <template>
-  <div>Fav</div>
+  <div class="catalog__grid">
+    <CatalogCard v-for="product in products" v-bind="product" :key="product.id" />
+  </div>
 </template>
+
+<style scoped>
+.catalog__grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 70px 24px;
+}
+</style>
