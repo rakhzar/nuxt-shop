eea2d9c7b27817f55c11fc9efed92f65285d4592
rakhzar <mnv36ru@gmail.com>
2026-04-09 08:54:57 +0300

    feat: enable persistence for favorites store

--- stores/favorites.store.ts
+++ stores/favorites.store.ts
@@ -1,29 +1,35 @@
 import { defineStore } from 'pinia';
 
-export const useFavoritesStore = defineStore('favorites', () => {
-  const favoriteIds = ref<number[]>([]);
+export const useFavoritesStore = defineStore(
+  'favorites',
+  () => {
+    const favoriteIds = ref<number[]>([]);
 
-  function addFavorite(id: number) {
-    if (!favoriteIds.value.includes(id)) {
-      favoriteIds.value.push(id);
+    function addFavorite(id: number) {
+      if (!favoriteIds.value.includes(id)) {
+        favoriteIds.value.push(id);
+      }
     }
-  }
 
-  function removeFromFavorite(id: number) {
-    favoriteIds.value = favoriteIds.value.filter((item) => item != id);
-  }
-
-  function isFavorite(id: number) {
-    return favoriteIds.value.find((f) => f == id);
-  }
-
-  function toggleFavorite(id: number) {
-    if (!favoriteIds.value.includes(id)) {
-      favoriteIds.value.push(id);
-      return;
+    function removeFromFavorite(id: number) {
+      favoriteIds.value = favoriteIds.value.filter((item) => item != id);
     }
-    favoriteIds.value = favoriteIds.value.filter((item) => item != id);
-  }
 
-  return { favoriteIds, addFavorite, removeFromFavorite, toggleFavorite, isFavorite };
-});
+    function isFavorite(id: number) {
+      return favoriteIds.value.find((f) => f == id);
+    }
+
+    function toggleFavorite(id: number) {
+      if (!favoriteIds.value.includes(id)) {
+        favoriteIds.value.push(id);
+        return;
+      }
+      favoriteIds.value = favoriteIds.value.filter((item) => item != id);
+    }
+
+    return { favoriteIds, addFavorite, removeFromFavorite, toggleFavorite, isFavorite };
+  },
+  {
+    persist: true,
+  }
+);
