7ae26bbb04740b4c1a8777fc19f0ec9daa68736d
rakhzar <mnv36ru@gmail.com>
2026-04-10 14:30:56 +0300

    feat: implement dynamic product page with SEO and API fetch

--- pages/catalog/[id].vue
+++ pages/catalog/[id].vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-const route = useRoute();
+import type { Product } from '~/interfaces/product.interface';
 
-const id = ref(route.params.id);
+const route = useRoute();
+const API_URL = useAPI();
+
+const { data } = await useFetch<{ product: Product }>(API_URL + '/products/' + route.params.id);
+
+useSeoMeta({
+  title: `Купить - ${data.value?.product.name}`,
+  description: data.value?.product.short_description,
+});
 </script>
 
 <template>
   <div>
-    <div>{{ id }}</div>
+    <div>{{ data?.product.name }}</div>
   </div>
 </template>
