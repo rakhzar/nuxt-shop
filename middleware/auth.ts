262817db2a64f3fdd8ec273cbc4a16ea61b35f97
rakhzar <mnv36ru@gmail.com>
2026-04-10 12:39:44 +0300

    refactor: fix cookie type definition and token access in auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware(() => {
-  const auth = useCookie<{ auth: string }>('auth');
+  const auth = useCookie<{ token: string }>('auth');
 
-  if (!auth.value) {
+  if (!auth.value?.token) {
     return navigateTo('/auth/login');
   }
 });
