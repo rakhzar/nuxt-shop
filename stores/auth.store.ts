8bd43054bf4a973d932db035ab47e0d29454e066
rakhzar <mnv36ru@gmail.com>
2026-04-11 20:07:37 +0300

    feat: add email state and setEmail method to auth store

--- stores/auth.store.ts
+++ stores/auth.store.ts
@@ -4,16 +4,21 @@ export const useAuthStore = defineStore(
   'auth',
   () => {
     const token = ref<string | undefined>();
+    const email = ref<string | undefined>();
 
     function setToken(value: string) {
       token.value = value;
+    }
+
+    function setEmail(value: string) {
+      email.value = value;
     }
 
     function clearToken() {
       token.value = undefined;
     }
 
-    return { token, setToken, clearToken };
+    return { token, email, setEmail, setToken, clearToken };
   },
   {
     persist: true,
