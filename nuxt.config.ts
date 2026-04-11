ce8edb95273c9c5b632799ee0e8a9b3a9e82a3d1
rakhzar <mnv36ru@gmail.com>
2026-04-11 20:35:26 +0300

    chore: enable Nuxt DevTools with timeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   runtimeConfig: {
     token: '',
     public: {
