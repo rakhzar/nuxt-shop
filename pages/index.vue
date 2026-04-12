2741124392b2dc79a333f31f4725955dc3ac17d3
rakhzar <mnv36ru@gmail.com>
2026-04-12 11:29:30 +0300

    refactor: restructure banner to BEM and add CTA button

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,18 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="banner-wrapper">
+  <div class="banner">
     <img class="banner-img" src="/assets/img/banner-img.svg" alt="Banner Image" />
+    <div class="banner__content">
+      <h1 class="banner__heading">Gold big hoops</h1>
+      <span class="banner__price">$ 68,00</span>
+      <NuxtLink class="banner__btn" to="/catalog">Смотреть</NuxtLink>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.banner-wrapper {
+.banner {
   position: relative;
   width: 100%;
   border-radius: 16px;
@@ -29,5 +34,43 @@ useSeoMeta({
   display: block;
   user-select: none;
   -webkit-user-drag: none;
+}
+
+.banner__content {
+  font-family: var(--font);
+  position: absolute;
+  top: 226px;
+  left: 39px;
+  color: var(--color-white-light);
+  display: flex;
+  flex-direction: column;
+}
+
+.banner__heading {
+  margin: 0;
+  color: var(--color-white-light);
+}
+
+.banner__price {
+  font-size: 26px;
+  margin-top: 16px;
+}
+
+.banner__btn {
+  height: 53px;
+  width: 193px;
+  margin-top: 48px;
+  padding: 14px 44px;
+  width: fit-content;
+  border: 3px solid var(--color-white-light);
+  border-radius: 6px;
+  color: var(--color-white-light);
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: none;
+}
+
+.banner__btn:hover {
+  background: rgba(255, 255, 255, 0.2);
 }
 </style>
